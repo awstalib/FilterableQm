3614fb435df8deae9dfc3fe67de0e668f7a75e38
aws <ous_sniper@yahoo.com>
2024-01-18 11:34:32 +0300

    Update FilterableQm.php

--- src/FilterableQm.php
+++ src/FilterableQm.php
@@ -16,7 +16,10 @@ trait FilterableQm
         // Loop through filters and apply each one to the query
         foreach ($filters as $key => $value) {
             // Extendable filter method if needed
-            $this?->extendedFilter($query, $key, $value);
+            //if extendedFilter method is not defined in the model then it will be ignored
+            if(method_exists($this,'extendedFilter')){
+                $this->extendedFilter($query, $key, $value);
+            }
 
             // Check if the key is allowed and the value is not empty
             if (in_array($key, $this->filterItems) && !empty($value)) {
